3947fede4c804e253e0b94e9ccbba892874de6fd
yunhai0417 <yunhai.wu@nestia.com>
2017-03-16 15:15:42 +0800

    删除 代理实现方案

--- YHPageView/CustomViewController.swift
+++ YHPageView/CustomViewController.swift
@@ -127,3 +127,30 @@ class CustomViewController: YHPageViewController ,YHPageViewHIddenBarDelegate{
     
    
 }
+
+
+extension CustomViewController{
+    
+    func pageScrollviewWillShow(_ fromIndex: Int, toIndex: Int, animated: Bool) {
+        
+    }
+    
+    func pageScrollviewDidShow(_ fromIndex: Int, toIndex: Int, finished: Bool) {
+        
+    }
+    
+    func pageScrollview(_ pageViewController: YHPageViewController, willTransitonFrom fromVC: UIViewController, toViewController toVC: UIViewController) {
+    }
+    
+    func pageScrollview(_ pageViewController: YHPageViewController, didTransitonFrom fromVC: UIViewController, toViewController toVC: UIViewController) {
+        
+    }
+    
+    func pageScrollview(_ pageViewController: YHPageViewController, didLeaveViewController fromVC: UIViewController, toViewController toVC: UIViewController, finished: Bool) {
+        
+    }
+    
+    func pageScrollview(_ pageViewController: YHPageViewController, willLeaveViewController fromVC: UIViewController, toViewController toVC: UIViewController, animated: Bool) {
+        
+    }
+}
